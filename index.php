3d282e26e0f6cdf461e104ac9ccd4f40097d7546
adelina-ok <72465533+adelina-ok@users.noreply.github.com>
2020-11-24 10:34:23 -0800

    updated index
    Added the $_GET syntax correctly.

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 //echo '<p>This is PHP!</p>';
 
-if(isset("FirstName")){//show feedback
- echo "FirstName"
+if(isset($_GET["FirstName"])){//show feedback
+ echo $_GET["FirstName"];
 }else{//show form
  echo '
  <form action="">
